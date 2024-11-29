eb409428729fcb7034adcc53618f354fd23577c0
bzzoo <fullbster@gmail.com>
2024-11-29 19:52:54 +0900

    [feat] QnA 투표 및 채택 기능 정의

--- core/core-domain/src/main/java/com/noname/domain/qna/application/AnswerService.java
+++ core/core-domain/src/main/java/com/noname/domain/qna/application/AnswerService.java
@@ -4,9 +4,11 @@ import com.noname.domain.common.LoginUser;
 import com.noname.domain.qna.application.implementation.AnswerReader;
 import com.noname.domain.qna.application.implementation.AnswerWriter;
 import com.noname.domain.qna.application.implementation.QuestionReader;
+import com.noname.domain.qna.application.implementation.VoteProcessor;
 import com.noname.domain.qna.domain.Answer;
 import com.noname.domain.qna.domain.AnswerContent;
 import com.noname.domain.qna.domain.Question;
+import com.noname.domain.qna.domain.VoteTarget;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +19,7 @@ public class AnswerService {
     private final QuestionReader questionReader;
     private final AnswerWriter answerWriter;
     private final AnswerReader answerReader;
+    private final VoteProcessor voteProcessor;
 
     public Long create(LoginUser user, Long questionId, AnswerContent content) {
         Question question = questionReader.getById(questionId);
@@ -31,6 +34,15 @@ public class AnswerService {
     public void delete(LoginUser user, Long answerId) {
         Answer answer = answerReader.getById(answerId);
         answerWriter.delete(user, answer);
+    }
 
+    public void upvote(LoginUser user, VoteTarget target) {
+        Answer answer = answerReader.getById(target.getId());
+        voteProcessor.upvote(user, answer, target);
+    }
+
+    public void downvote(LoginUser user, VoteTarget target) {
+        Answer answer = answerReader.getById(target.getId());
+        voteProcessor.upvote(user, answer, target);
     }
 }
--- core/core-domain/src/main/java/com/noname/domain/qna/application/implementation/QuestionWriter.java
+++ core/core-domain/src/main/java/com/noname/domain/qna/application/implementation/QuestionWriter.java
@@ -2,6 +2,7 @@ package com.noname.domain.qna.application.implementation;
 
 import com.noname.domain.common.LoginUser;
 import com.noname.domain.qna.application.port.QuestionRepository;
+import com.noname.domain.qna.domain.Answer;
 import com.noname.domain.qna.domain.Question;
 import com.noname.domain.qna.domain.QuestionContent;
 import com.noname.domain.qna.domain.QuestionTitle;
@@ -37,11 +38,15 @@ public class QuestionWriter {
         questionRepository.save(question);
     }
 
-
     public void delete(LoginUser user, Question question) {
         answerRestrictionValidator.validateQuestionDelete(question);
         voteRestrictionValidator.validateQuestionUpdatable(user, question);
         question.delete(user.id());
+        questionRepository.save(question);
+    }
+
+    public void adopt(LoginUser user, Question question, Answer answer){
+        question.adoptAnswer(user.id(), answer.getId());
         questionRepository.save(question);
     }
 }
